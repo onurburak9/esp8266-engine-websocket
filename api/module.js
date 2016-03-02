23eeafd781763a4c9c884df58f09926cca5a114d
Onur <onurburak@sabanciuniv.edu>
2016-03-02 11:22:14 +0200

    ready for test env

--- api/module.js
+++ api/module.js
@@ -1,4 +1,4 @@
-var Api = function(Instagram, express, store, bodyParser, Bot) {
+var Api = function(Instagram, express, store, bodyParser, Bot, CronJob) {
 	var instance;
 
 	function init() {
@@ -6,35 +6,56 @@ var Api = function(Instagram, express, store, bodyParser, Bot) {
 
 		var app = express();
 		app.set('port', process.env.PORT || 3000);
-		app.use(bodyParser());
+		app.use(bodyParser.json()); // to support JSON-encoded bodies
+		app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+			extended: true
+		}));
 
 
 		app.get('/getByTagName', function(req, res) {
+			Instagram.set('access_token', '20581734.1fb234f.d1ee7fe4bb524aa4b2d9fda9a4372259');
+
 			var tag = req.query.tag;
 			Instagram.tags.recent({
 				name: tag,
 				complete: function(data) {
-					console.log(data);
+
+					res.send({
+						code: 200,
+						message: 'SUCCESS',
+						obj: data
+					})
 				}
 			});
 
-			var obj = {}
 
-			res.send({
-				code: 200,
-				message: 'SUCCESS',
-				obj: obj
-			})
 		});
 
 		//Auth
 		app.get('/auth', Bot.authorize);
-
 		//Callback for auth
 		app.get('/auth/callback', Bot.authorizeCallback);
 
 		//Add Tag
-		app.post('/addTag',Bot.addTag);
+		app.post('/addTag', Bot.addTags);
+		//Remove Tag
+		app.delete('/removeTag', Bot.removeTag);
+
+
+		app.get('/like', Bot.autoLike);
+
+		//Job got autoLike
+		// var job = new CronJob('* * * * *', function() {
+		// 		var currentdate = new Date();
+		// 		var datetime = "Last Sync: " + currentdate.getDate() + "/" + (currentdate.getMonth() + 1) + "/" + currentdate.getFullYear() + " @ " + currentdate.getHours() + ":" + currentdate.getMinutes() + ":" + currentdate.getSeconds();
+		// 		console.log(datetime);
+		// 	}, function() {
+		// 		console.log('/nONUR')
+		// 	},
+		// 	true, /* Start the job right now */
+		// 	'Europe/Istanbul' /* Time zone of this job. */
+		// );
+
 
 
 		return app;
--- server.js
+++ server.js
@@ -7,7 +7,8 @@ var express = require('express'),
 	async = require('async'),
 	bodyParser = require('body-parser'),
 	Q = require('q'),
-	mongoSkin = require('mongoskin');
+	mongoSkin = require('mongoskin'),
+	CronJob = require('cron').CronJob;
 
 Instagram.set('client_id', '5c4891f0cb6f4865b818d4b40d17edde');
 Instagram.set('client_secret', '8b2f847113254893a7cc13f725dfbfe7');
@@ -31,10 +32,15 @@ var StoreFactory = require(path.join('lib', 'StoreFactory'));
 var storeFactory = new StoreFactory();
 var store = storeFactory.getStore(Q, async, _, mongoDb);
 
-var Bot = require(path.join('lib', 'bot'))(Instagram,store).getInstance();
+var Bot = require(path.join('lib', 'bot'))(Instagram, store, _, Q).getInstance();
 
-var app = require(path.join('api'))(Instagram,express,store,bodyParser,Bot).getInstance();
+var app = require(path.join('api'))(Instagram, express, store, bodyParser, Bot, CronJob).getInstance();
+
+//Create here a cronjob that call Bot.autoLike
 
 http.createServer(app).listen(app.get('port'), function() {
 	console.log('Express server listening on port ', app.get('port'));
+	var currentdate = new Date();
+	var datetime = "Last Sync: " + currentdate.getDate() + "/" + (currentdate.getMonth() + 1) + "/" + currentdate.getFullYear() + " @ " + currentdate.getHours() + ":" + currentdate.getMinutes() + ":" + currentdate.getSeconds();
+	console.log(datetime);
 });
