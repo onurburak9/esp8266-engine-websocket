2f011493354cf810a2f58dcbdc47ebba283627dc
Onur <onurburak@sabanciuniv.edu>
2017-03-07 12:59:47 -0800

    port adj

--- api/module.js
+++ api/module.js
@@ -5,7 +5,7 @@ var Api = function(express, bodyParser, path, expressApp, io,http,qs) {
 
 
 		var app = expressApp;
-		app.set('port', process.env.PORT || 3000);
+		app.set('port', process.env.PORT || 3001);
 		app.use(bodyParser.json()); // to support JSON-encoded bodies
 		app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
 			extended: true
